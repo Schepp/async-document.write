b44b64ba4b94e8acb8e8eea12d5a2fc3914ece32
Christian Schaefer <schaepp@gmx.de>
2020-05-21 07:46:49 +0200

    Improves compatibility
    * Corrects order of output when multiple writes happen in the same script.
    * Takes text nodes into account when determining the right place to write.
    * Drops the use of a <span> as write container.
    * Adds more tests.

--- async-document.write.js
+++ async-document.write.js
@@ -1,22 +1,38 @@
 // Async document.write by Christian "Schepp" Schaefer (@derSchepp)
 // MIT license
 (function () {
+  if (
+    !document.currentScript ||
+    !(document.createRange()).createContextualFragment ||
+    !window.Map
+  ) {
+    return;
+  }
+
   var originalDocumentWrite = document.write;
+  var nextSiblings = new Map();
 
   document.write = function (html) {
     var sourceScript = document.currentScript;
     var range = document.createRange();
 
-    if (!sourceScript || !range.createContextualFragment) {
+    if (!sourceScript) {
       originalDocumentWrite.apply(document, [html]);
       return;
     }
 
     var parentElement = sourceScript.parentElement;
-    var span = document.createElement('span');
+    var nextSibling = nextSiblings.get(sourceScript);
 
-    range.setStart(span, 0);
-    parentElement.insertBefore(span, sourceScript.nextElementSibling);
-    span.appendChild(range.createContextualFragment(html));
+    if (nextSiblings === undefined) {
+      nextSibling = sourceScript.nextSibling;
+      nextSiblings.set(sourceScript, nextSibling);
+    }
+
+    var fragment = document.createDocumentFragment();
+
+    range.setStart(fragment, 0);
+    fragment.appendChild(range.createContextualFragment(html));
+    parentElement.insertBefore(fragment, nextSibling);
   };
 })();
---
+++ test.js
@@ -0,0 +1,1 @@
+document.write('<p>external document.write</p>');
